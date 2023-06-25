c3d328a1419e8020383df3efe72a0cec4e8a040c
yuns <yuns.xie@qq.com>
2023-06-25 19:58:12 +0800

    chore: update hint

--- configure.zsh
+++ configure.zsh
@@ -2,6 +2,7 @@
 
 cd "$HOME/.wslboot" || exit
 
+# zsh 下重装 nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 source "$HOME/.zshrc"
 
@@ -13,4 +14,4 @@ nr zsh:configure
 # 不生效，需要用户手动执行生效
 # source "$HOME/.zshrc"
 
-echo "Congratuation! WSL configuration completed."
+echo "恭喜~ WSL 配置初始化已完成"
